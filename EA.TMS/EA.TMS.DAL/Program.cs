6eaffefce284377d6bb019696270390808642808
Duy <duyhthai@gmail.com>
2019-01-09 17:36:25 +0700

    DataAccess layer

---
+++ EA.TMS/EA.TMS.DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EA.TMS.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
